b8146058a97ec83c2b639d1f7b08a8a22c11b6d3
frllk <1402805300@qq.com>
2020-06-10 01:27:47 +0800

    layout布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class=''>Layout</div>
+  <div class='container'>
+    <van-nav-bar
+      title="黑马头条"
+      right-text="搜索"
+      left-arrow
+      @click-left="$router.push('/login')"
+      @click-right="$router.push('/search')"
+    />
+    <!-- 二级路由入口 -->
+    <router-view></router-view>
+    <van-tabbar>
+    <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+    <van-tabbar-item to="/question" icon="chat-o">问答</van-tabbar-item>
+    <van-tabbar-item to="/video" icon="video-o">视频</van-tabbar-item>
+    <van-tabbar-item to="/user" icon="user-o">我的</van-tabbar-item>
+  </van-tabbar>
+  </div>
 </template>
 
 <script>
