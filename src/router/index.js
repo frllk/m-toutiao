c104d46e1b29c28aa6186d74ab09d8fb195f0a4e
frllk <1402805300@qq.com>
2020-06-10 01:14:31 +0800

    整体路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,20 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Layout
+    component: Layout,
+    children: [
+      { path: '/', component: () => import('@/views/home') }, // 二级路由主页
+      { path: '/question', component: () => import('@/views/question') }, // 问答
+      { path: '/video', component: () => import('@/views/video') }, // 视频
+      { path: '/user', component: () => import('@/views/user') } // 个人中心
+    ]
   },
-  { path: '/login', component: LoginIndex }
+  { path: '/login', component: LoginIndex }, // 登录页
+  { path: '/user/profile', component: () => import('@/views/user/profile.vue') }, // 编辑资料
+  { path: '/user/chat', component: () => import('@/views/user/chat.vue') }, // 小智同学
+  { path: '/search', component: () => import('@/views/search') }, // 搜索中心
+  { path: '/search/result', component: () => import('@/views/search/result') }, // 搜索结果
+  { path: '/article', component: () => import('@/views/article') } // 文章详情
   // {
   //   path: '/about',
   //   name: 'About',
