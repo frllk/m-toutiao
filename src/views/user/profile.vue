805d3b14f9a4bcae5032f5ad663360de54ded250
frllk <1402805300@qq.com>
2020-06-16 10:56:33 +0800

    个人中心---编辑资料组件及布局

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -1,22 +1,52 @@
 <template>
-  <div class=''>profile</div>
+  <div class="container">
+    <!-- 1、顶部导航条 -->
+    <van-nav-bar left-arrow @click-left="$router.back()" title="编辑资料"></van-nav-bar>
+    <!-- 2、具体修改项 -->
+    <van-cell-group>
+      <van-cell is-link title="头像"  center>
+        <van-image
+          slot="default"
+          width="1.5rem"
+          height="1.5rem"
+          fit="cover"
+          round
+          :src="user.photo"
+        />
+      </van-cell>
+      <van-cell is-link title="名称" :value="user.name" @click="showName=true"/>
+      <van-cell is-link title="性别" :value="user.gender === 1?'男':'女'" @click="showGender=true"/>
+      <van-cell is-link title="生日" :value="user.birthday" @click="showBirthday=true"/>
+    </van-cell-group>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'MyComponent',
-  props: [],
-  components: {},
+  name: 'userProfile',
   data () {
-    return {}
-  },
-  methods: {
+    return {
+      // 控制弹层
+      showName: false,
+      showGender: false,
+      showBirthday: false,
 
-  },
-  computed: {},
-  created () {},
-  mounted () {}
+      // 当前用户的信息
+      user: {},
+
+      // 修改后的新名字
+      newName: '',
+      // 修改后新生日
+      newDate: new Date(),
+      minDate: new Date(1965, 0, 10), // dateTime-picker中最小时间
+      maxDate: new Date() // 当前时间
+    }
+  }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style lang="less">
+.van-nav-bar__text{
+  color:#fff;
+}
+</style>
